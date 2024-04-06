59fc155b4aeb9d9130c9ada9cda3cf24a29fbf38
freakinward <aaron.l.ward@gmail.com>
2024-04-06 17:27:49 -0500

    feat(ServiceArea): will now fetch service icons from CMS

--- src/Features/Home/components/Services.tsx
+++ src/Features/Home/components/Services.tsx
@@ -1,13 +1,6 @@
-import useServiceArea from '../../hooks/useServiceArea';
+import Image from 'next/image';
 
-const serviceIconMap = {
-  WordPress: 'fa fa-wordpress',
-  CartArrowDown: 'fa fa-cart-arrow-down',
-  CloudDownload: 'fa fa-cloud-download',
-  Language: 'fa fa-language',
-  Plane: 'fa fa-plane',
-  PieChart: 'fa fa-pie-chart',
-};
+import useServiceArea from '../../hooks/useServiceArea';
 
 export default function Services() {
   const { data: serviceArea } = useServiceArea();
@@ -22,7 +15,14 @@ export default function Services() {
         <div className="row">
           {serviceArea?.services?.map(service => (
             <div key={`${service.name}-${service.id}`} className="col-md-4">
-              <i className={serviceIconMap[service.icon]}></i>
+              <i className="fa">
+                <Image
+                  src={service.image.url}
+                  width={service.image.width}
+                  height={service.image.height}
+                  alt={service.image.alternateText}
+                />
+              </i>
               <div className="service-desc">
                 <h3>{service.name}</h3>
                 <p>{service.text}</p>

48723e7da2593061d443e6d9143e09ff92c25059
freakinward <aaron.l.ward@gmail.com>
2024-09-01 19:21:14 -0500

    chore: update from cms v1-cleanup

--- codegen.ts
+++ codegen.ts
@@ -2,8 +2,7 @@ import { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  // schema: 'http://127.0.0.1:8080/graphql',
-  schema: 'https://cms-strapi-server.azurewebsites.net/graphql',
+  schema: 'http://127.0.0.1:8080/graphql',
   generates: {
     './types/strapi/__generated__/': {
       preset: 'client',
